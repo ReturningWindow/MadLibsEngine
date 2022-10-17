e9eef5f9109bb34b0937e8c3a7bea800915bea34
Window <walkerrob302@gmail.com>
2022-10-16 17:13:35 -0700

    Adds documentation to mad_libs_handler.py
    Includes a well documented explanations of the code written all the way
    from constructor to the get_mad_libs function.

--- Halloween Mad Libs/mad_libs_handler.py
+++ Halloween Mad Libs/mad_libs_handler.py
@@ -3,6 +3,18 @@ class MadLibsHandler:
     COMMANDS = ["NOUN", "VERB", "ADJECTIVE", "DONE"]
 
     def __init__(self, name, script, commands=[]):
+        """Creates a MadLibsHandler object
+
+        Parameters:
+        name: str
+            The name of the mad libs.
+
+        script: str
+            The text of the mad libs game.
+
+        commands: list of str, optional
+            The commands registered for the game to play.
+        """
         self.name = name
         self.script = script
         self.command = None
@@ -11,9 +23,27 @@ class MadLibsHandler:
         self.words = {}
 
     def set_commands(self, commands):
+        """Overrides the preivous commands with new commands.
+
+
+        Parameters:
+            commands: list of str
+            A list of commands for the writers of the game.
+
+        Returns:
+            None
+        """
         self.registered_commands = commands
 
     def create_mad_libs(self):
+        """Allows a creator to input a list of commands manually until DONE is typed.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         print(
             "The valid commands here are NOUN, VERB, ADJECTIVE, and DONE. You can get plurals by adding s to the commands that aren't DONE"
         )
@@ -27,6 +57,21 @@ class MadLibsHandler:
         print("Commands registered!")
 
     def play_mad_libs(self):
+        """Allows the player to play a game of madlibs
+
+        Parameters:
+            None
+
+        Raises:
+            Exception #1
+                If command "DONE" is read into the commands it will break the program.
+
+            Exception #2
+                If there is no command to give, then it will break the program and not proceed.
+        Returns:
+            None
+        """
+
         for index, command in enumerate(self.registered_commands):
             match command:
                 case "noun":
@@ -42,7 +87,7 @@ class MadLibsHandler:
                 case "done":
                     raise Exception(f"Invalid {command} registered in commands.")
                 case "":
-                    raise Exception(f"No command received for commands.")
+                    raise Exception(f"No command received for this entry.")
                 case _:
                     word = input(f"Enter a {command}: ")
 
@@ -53,4 +98,14 @@ class MadLibsHandler:
                 self.words[index + 1] = word
 
     def get_mad_libs(self):
+        """Gets the end result of the game for the user.
+
+        Parameters:
+            None
+
+        Returns
+            str
+                A script full of what the user inputted into your game of mad libs.
+        """
+
         return self.script.format(*self.words.values())
